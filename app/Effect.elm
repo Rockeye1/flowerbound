7ba946166f04bef19e967ef8892aca6e9250ea44
Leonardo Taglialegne <cmt.miniBill@gmail.com>
2024-08-20 17:49:08 +0200

    Parsererror to string

--- app/Effect.elm
+++ app/Effect.elm
@@ -9,9 +9,12 @@ module Effect exposing (Effect(..), batch, fromCmd, map, none, perform, rollChec
 import Browser.Navigation
 import File exposing (File)
 import File.Select
+import Json.Encode
+import List.Extra
 import Parser
 import Persona.Codec
 import Random
+import Set
 import Task
 import Types exposing (Persona)
 
@@ -145,5 +148,102 @@ perform ({ fromPageMsg, key } as helpers) effect =
 
 
 parserErrorToString : String -> List Parser.DeadEnd -> String
-parserErrorToString _ _ =
-    "TODO: parserErrorToString"
+parserErrorToString input err =
+    err
+        |> List.Extra.gatherEqualsBy (\{ row, col } -> ( row, col ))
+        |> List.map (errorToString input)
+        |> String.join "\n\n"
+
+
+errorToString : String -> ( Parser.DeadEnd, List Parser.DeadEnd ) -> String
+errorToString source ( error, errors ) =
+    let
+        -- How many lines of context to show
+        context : Int
+        context =
+            4
+
+        lines : List String
+        lines =
+            String.split "\n" source
+                |> List.drop (error.row - context)
+                |> List.take (context * 2)
+
+        errorString : String
+        errorString =
+            [ String.repeat (error.col - 1) " "
+            , {- Console.red -} "^ "
+            , " at row "
+            , String.fromInt error.row
+            , ", col "
+            , String.fromInt error.col
+            , " "
+            , (error :: errors)
+                |> List.map (\{ problem } -> problemToString problem)
+                |> Set.fromList
+                |> Set.toList
+                |> String.join ", "
+
+            -- |> Console.yellow
+            ]
+                |> String.concat
+
+        before : Int
+        before =
+            min error.row context
+    in
+    List.take before lines
+        ++ errorString
+        :: List.drop before lines
+        |> String.join "\n"
+
+
+problemToString : Parser.Problem -> String
+problemToString problem =
+    case problem of
+        Parser.BadRepeat ->
+            "Bad repeat"
+
+        Parser.Expecting something ->
+            "Expecting " ++ something
+
+        Parser.ExpectingInt ->
+            "Expecting int"
+
+        Parser.ExpectingHex ->
+            "Expecting hex"
+
+        Parser.ExpectingOctal ->
+            "Expecting octal"
+
+        Parser.ExpectingBinary ->
+            "Expecting binary"
+
+        Parser.ExpectingFloat ->
+            "Expecting float"
+
+        Parser.ExpectingNumber ->
+            "Expecting number"
+
+        Parser.ExpectingVariable ->
+            "Expecting variable"
+
+        Parser.ExpectingSymbol value ->
+            "Expecting symbol " ++ escape value
+
+        Parser.ExpectingKeyword value ->
+            "Expecting keyword " ++ escape value
+
+        Parser.ExpectingEnd ->
+            "Expecting end"
+
+        Parser.UnexpectedChar ->
+            "Unexpected char"
+
+        Parser.Problem p ->
+            p
+
+
+escape : String -> String
+escape input =
+    Json.Encode.encode 0 (Json.Encode.string input)
